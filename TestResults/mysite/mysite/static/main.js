61a14676f5efb2b91909f3e82367a9de254bbb57
Yen-Hsun, Huang <s011208@gmail.com>
2016-01-28 23:57:08 +0800

    remove *.pyc and add animate height query

--- TestResults/mysite/mysite/static/main.js
+++ TestResults/mysite/mysite/static/main.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var collapse_height=70;
+    var expand_height=300;
+    var expand_delta=expand_height-collapse_height;
+    var expand_animate_time=400;
 	$("tr.content_column").click(function () {
 		$(this).find(".content_failure").each(function() {
-			if($(this).height() == 70) {
-				$(this).css('height', '100%');
+			if($(this).height() == collapse_height) {
+                $(this).animate({height:expand_delta},expand_animate_time);
 			} else {
-				$(this).css('height', '70px');
+				$(this).animate({height:collapse_height},expand_animate_time);
+			}
+		});
+		$(this).find("td.column div.column").each(function() {
+			if($(this).height() == collapse_height) {
+                $(this).animate({height:expand_delta},expand_animate_time);
+			} else {
+                $(this).animate({height:collapse_height},expand_animate_time);
 			}
 		});
 	});

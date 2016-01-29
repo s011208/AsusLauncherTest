8f48e52d38453fb63771e0b77a4cf130e0f17cd6
Yen-Hsun_Huang <yen-hsun_huang@asus.com>
2016-01-29 11:25:24 +0800

    add fail rate & fix AsusLauncher tag issue

--- TestResults/mysite/tests/templatetags/test_library.py
+++ TestResults/mysite/tests/templatetags/test_library.py
@@ -21,10 +21,21 @@ def get_test_version(test_versions, test_time_id):
 def get_test_tag(test_tags, test_time_id):
     for tag in test_tags:
         if int(tag.get('test_time_id')) == int(test_time_id):
-            return tag.get('test_version')
+            return tag.get('test_tag')
     return "N/A"
-    
+	
+def get_test_case_fail_rate(test_results, test_case_id):
+    total_results = 0
+    pass_results = 0
+    for result in test_results:
+        if int(result.get('test_case_id')) == int(test_case_id):
+           total_results += 1
+           if result.get('test_result') == 'OK':
+               pass_results += 1
+    return int(round(1 - float(pass_results) / float(total_results), 2) * 100)
+
 register.simple_tag(remove_pkg)
 register.simple_tag(get_test_result)
 register.simple_tag(get_test_version)
 register.simple_tag(get_test_tag)
+register.simple_tag(get_test_case_fail_rate)
